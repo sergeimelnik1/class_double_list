4c6f24da122cab6e6267772b0de0c6e656042151
sergeimelnik1 <sergeimelnik-1@yandex.ru>
2016-10-29 09:59:19 +0300

    make new code

--- class_double_list/class_double_list.h
+++ class_double_list/class_double_list.h
@@ -1,24 +1,29 @@
+#pragma once
+#include <iostream>
 using namespace std;
 
 class double_linked_list {
 private:
 
-struct Node{
-    int data;
-    struct List *next;
-    struct List *prev;
-};
-    Node* head, tail;
-    
+	struct Node {
+		int data;
+		struct Node *next;
+		struct Node *prev;
+	};
+	Node* head,* tail;
+
 public:
-void *createlist(int data);
-void printlist(struct Node *first);
-void * addTobegin(Node*lst, int number);
-void * addToend(Node*lst, int number);
-void addTomiddle(List**lst, int number, int after);
-void deleteElem(List**lst,int number);
-void *deletehead(Node*root);
-void *deletend(Node*root);
-void * search(Node*node, int val);
-void sort(Node**head);
+
+	double_linked_list();
+	~double_linked_list();
+	bool isempty();
+	void printlist();
+	void append(int number);
+	void add(int number);
+	void deletehead();
+	void deletend();
+	Node* search(int val);
+	void deleteElem(int number);
+	void insert(int number, int after);
+	void sort();
 };
