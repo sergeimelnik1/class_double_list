cb315e9fc1fd493350fa16c68a618cb76d0d631a
sergeimelnik1 <sergeimelnik-1@yandex.ru>
2016-10-29 09:58:27 +0300

    Update class_double_list.cpp
    made new code

--- class_double_list/class_double_list.cpp
+++ class_double_list/class_double_list.cpp
@@ -1,104 +1,114 @@
+#include "stdafx.h"
 #include "iostream"
-#include "class_double_list.h"
+#include "double_line.h"
 
-void *createlist(int data){
-    Node *n = new Node;
-    n->data = data;
-    n->next = NULL;
-    n->prev = NULL;
-    return n;
+struct List *createlist(int data){
+	List*n = new List;
+	n->data = data;
+	n->next = NULL;
+	n->prev = NULL;
+	return n;
 }
 
-void printlist(struct Node *first){
-    Node*temp = first;
-    while (temp != NULL){
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
+void printlist(struct List *first){
+	List*temp = first;
+	while (temp != NULL){
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
 }
 
-void * search(Node*node, int val){
-    while (node != NULL && node->data != val){
-        node = node->next;
-    }
-    return node;
+struct List * search(List*node, int val){
+	while (node != NULL && node->data != val){
+		node = node->next;
+	}
+	return node;
 }
 
-void * addToend(Node*lst, int number){
-    struct Node *temp, *p;
-    temp = new Node;
-    p = lst->next;
-    lst->next = temp;
-    temp->data = number;
-    temp->next = p;
-    temp->prev = lst;
-    if (p != NULL){
-        p->prev = temp;
-    }
-    return temp;
+struct List * addToend(List*lst, int number){
+	struct List *temp, *p;
+	temp = new List;
+	p = lst->next;
+	lst->next = temp;
+	temp->data = number;
+	temp->next = p;
+	temp->prev = lst;
+	if (p != NULL){
+		p->prev = temp;
+	}
+	return temp;
 }
 
-void * addTobegin(Node*lst, int number){
-    Node*n = new Node;
-    n->next = lst;
-    n->prev = NULL;
-    n->data = number;
-    lst = n;
-    return n;
+struct List * addTobegin(List*lst, int number){
+	List*n = new List;
+	n->next = lst;
+	n->prev = NULL;
+	n->data = number;
+	lst = n;
+	return n;
 }
 
-void *deletehead(Node*root){
-    struct Node*temp;
-    temp = root->next;
-    temp->prev = NULL;
-    delete(root);
-    return temp;
+struct List *deletehead(List*root){
+	struct List*temp;
+	temp = root->next;
+	temp->prev = NULL;
+	delete(root);
+	return temp;
 }
 
-void *deletend(Node*root){
-    struct Node*temp;
-    temp = root->prev;
-    temp->next = NULL;
-    delete(root);
-    return temp;
+struct List *deletend(List*root){
+	struct List*temp;
+	temp = root->prev;
+	temp->next = NULL;
+	delete(root);
+	return temp;
 }
 
-void deleteElem(Node**lst, int number){
-    struct Node * node = search(*lst, number);
-    if (node != NULL) {
-        struct Node*p = node->prev;
-        struct Node*n = node->next;
-        p->next = n;
-        n->prev = p;
-        delete node;
-    }
-    else cout << "there is no such element";
+void deleteElem(List**lst, int number){
+	struct List * node = search(*lst, number);
+	if (node != NULL) {
+	     struct List*p = node->prev;
+	     struct List*n = node->next;
+	     p->next = n;
+	     n->prev = p;
+	     delete node;
+	}
+	else cout << "there is no such element";
 }
 
-void addTomiddle(Node**lst, int number, int after){
-    struct Node * node = search(*lst,after);
-    if (node != NULL) {
-        Node*n = new Node;
-        struct Node*p = node->next;
-        node->next = n;
-        n->next = p;
-        p->prev = n;
-    }
-    else cout << "there is no such element";
+void addTomiddle(List**lst, int number, int after){
+	struct List * node = search(*lst,after);
+	if (node != NULL) {
+		List*n = new List;
+	        struct List*p = node->next;
+	        node->next = n;
+		n->data=number;
+		n->prev = node;
+	        n->next = p;
+	        p->prev = n;
+	}
+	else cout << "there is no such element";
 }
 
-void sort(Node**head) {
-    Node* temp = *head;
-    while (temp->next) {
-        if (temp->data > temp->next->data) {
-            int c = temp->data;
-            temp->data = temp->next->data;
-            temp->next->data = c;
-            temp = temp->next;
-            sort(head);
-        }
-        else
-            temp = temp->next;
-    }	  
+void sort(List**head) {
+	List* temp = *head;
+		while (temp->next) {
+			if (temp->data > temp->next->data) {
+				int c = temp->data;
+				temp->data = temp->next->data;
+				temp->next->data = c;
+				temp = temp->next;
+				sort(head);
+			}
+			else
+				temp = temp->next;
+		}	  
+}
+void memory_clear(List*head) {
+	while (head) {
+		List* tmp = head->next; 
+		delete head; 
+		head = tmp; 
+	}
 }
